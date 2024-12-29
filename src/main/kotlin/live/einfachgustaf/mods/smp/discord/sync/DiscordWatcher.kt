8d5e25f2f98221d7747194519af2a6e7d940a33d
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-12-29 12:13:03 +0100

    add minecraft mentions

--- src/main/kotlin/live/einfachgustaf/mods/smp/discord/sync/DiscordWatcher.kt
+++ src/main/kotlin/live/einfachgustaf/mods/smp/discord/sync/DiscordWatcher.kt
@@ -4,6 +4,9 @@ import dev.kord.core.Kord
 import dev.kord.core.entity.ReactionEmoji
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.on
+import live.einfachgustaf.mods.smp.extensions.sendNotifcation
+import net.minecraft.advancements.AdvancementType
+import net.minecraft.world.item.Items
 import net.silkmc.silk.core.Silk
 import net.silkmc.silk.core.text.broadcastText
 import net.silkmc.silk.core.text.literalText
@@ -20,11 +23,28 @@ class DiscordWatcher(val parent: ChatSync) {
 
             MinecraftWatcher.lastName = ""
 
+            var buildMessage = message.content
+            val mcPrefix = "@mc:"
+            val regex = Regex("$mcPrefix\\w+")
+            val matches = regex.findAll(buildMessage)
+
+            if (matches.any())
+                matches.forEach { match ->
+                    val playerName = match.value.removePrefix(mcPrefix)
+
+                    val query = Silk.server?.playerList?.getPlayerByName(playerName)
+
+                    if (query != null) {
+                        buildMessage = buildMessage.replace(match.value, playerName)
+                        query.sendNotifcation(literalText("Du wurdest von ${member?.username ?: "Unknown"} in Discord erwähnt!") {  }, icon = Items.RED_DYE.defaultInstance, type = AdvancementType.GOAL)
+                    }
+                }
+
             Silk.server?.broadcastText(literalText {
                 text("[Discord] ")
                 text(member?.username ?: "Unknown")
                 text(": ")
-                text(message.content)
+                text(buildMessage)
             })
 
             message.addReaction(ReactionEmoji.Unicode("🟢"))
