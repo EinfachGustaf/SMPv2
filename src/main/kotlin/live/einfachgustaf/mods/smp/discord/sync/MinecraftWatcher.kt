a408af7e0dfa791adeedeb0f64d1a8c6c6d9e85c
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-12-29 11:53:39 +0100

    add ping check

--- src/main/kotlin/live/einfachgustaf/mods/smp/discord/sync/MinecraftWatcher.kt
+++ src/main/kotlin/live/einfachgustaf/mods/smp/discord/sync/MinecraftWatcher.kt
@@ -6,6 +6,7 @@ import dev.kord.core.entity.channel.TextChannel
 import kotlinx.coroutines.launch
 import net.fabricmc.fabric.api.message.v1.ServerMessageEvents
 import net.silkmc.silk.core.task.mcCoroutineScope
+import net.silkmc.silk.core.text.literalText
 
 class MinecraftWatcher(val parent: ChatSync) {
 
@@ -13,8 +14,13 @@ class MinecraftWatcher(val parent: ChatSync) {
 
     fun hook(kord: Kord) {
         // TODO: forbidden pings like (everyone, here, roles)
-        // TODO: ping user (@dc:username)
         ServerMessageEvents.CHAT_MESSAGE.register { message, player, _ ->
+            if (message.signedContent().contains("@")) {
+                player.sendSystemMessage(literalText("You are not allowed to mention Discord users!") {
+                    color = 0xFF0000
+                })
+                return@register
+            }
             mcCoroutineScope.launch {
                 (kord.getChannel(parent.channelSnowflake) as TextChannel).createMessage {
                     content = buildString {
