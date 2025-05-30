f2b9d1099fd6645510a28198016900efb813df8d
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2025-05-30 18:03:45 +0200

    Add Shadow plugin and update JDA dependency scope in build.gradle.kts

--- smp-discord/build.gradle.kts
+++ smp-discord/build.gradle.kts
@@ -1,10 +1,18 @@
 plugins {
     alias(libs.plugins.jvm)
     alias(libs.plugins.userdev)
+    alias(libs.plugins.shadow)
 }
 
 dependencies {
     paperweight.paperDevBundle(libs.versions.paper)
     compileOnly(project(":smp-core"))
-    compileOnly(libs.jda)
+    implementation(libs.jda)
+}
+
+// TODO: use Paper Dependency Loader
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
 }
